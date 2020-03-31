75f022f1d2ffe10235e7d00a3c5543775dc19d31
kedrboje <fmrubchenkov@edu.hse.ru>
2020-03-31 13:29:29 +0300

    Initial Commit

---
+++ Eventer 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventer 2.0
+//
+//  Created by Федор Рубченков on 31.03.2020.
+//  Copyright © 2020 Федор Рубченков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
